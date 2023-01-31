c3a422e523c95ba47b906ba52805f911834e68e1
Farid Amimri <farid.amimri@gmail.com>
2023-01-31 09:38:57 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import styled from 'styled-components'
+
+function Navbar() {
+  return (
+    <div>
+      <Left></Left>
+    </div>
+  )
+}
+
+export default Navbar
+
+const Left = styled.div``
