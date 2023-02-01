d63972865bb280cf8ea87412f3722ec1a6cae8be
Farid Amimri <farid.amimri@gmail.com>
2023-02-01 11:50:17 +0100

    Add Service section

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import './global.css'
 import Navbar from './components/Navbar'
 import Intro from './components/Intro'
+import Service from './components/Service'
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <Intro />
+      <Service />
     </div>
   )
 }
