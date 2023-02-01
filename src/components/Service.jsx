b4e80e19f7ee52744db2ead8397bbaf3c7afaf28
Farid Amimri <farid.amimri@gmail.com>
2023-02-01 11:49:11 +0100

    Create Service component

---
+++ src/components/Service.jsx
@@ -0,0 +1,71 @@
+/** @format */
+
+import '../global.css'
+import styled from 'styled-components'
+import { serviceData } from '../data'
+
+function Service() {
+  return (
+    <Container>
+      <Header>Obtenez Votre Projet De Rêve en 3 étapes</Header>
+      <Services>
+        {serviceData.map((item) => (
+          <ServiceItem key={item.id}>
+            <Picto>{item.picto}</Picto>
+            <Title> {item.title}</Title>
+            <Description> {item.description}</Description>
+          </ServiceItem>
+        ))}
+      </Services>
+    </Container>
+  )
+}
+
+export default Service
+
+const Container = styled.div`
+  height: calc(100vh - 100px);
+  padding: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Header = styled.h1`
+  text-align: center;
+  font-size: 48px;
+`
+const Services = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const ServiceItem = styled.div`
+  width: 300px;
+  height: 232px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background-color: var(--background-color-secondary);
+  border-radius: 20px;
+  padding: 30px;
+`
+
+const Picto = styled.div`
+  width: 56px;
+  height: 56px;
+  color: var(--text-color-primary);
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const Title = styled.h2`
+  font-size: 24px;
+`
+
+const Description = styled.p`
+  font-size: 16px;
+`
