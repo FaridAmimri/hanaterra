8f7a1abb890ddce71712b21b7295ad162e402d38
Farid Amimri <farid.amimri@gmail.com>
2023-02-01 11:50:47 +0100

    Add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,14 @@
 import '../global.css'
 import styled from 'styled-components'
 import MenuIcon from '@mui/icons-material/Menu'
+import LogoPng from '../assets/logo.png'
 
 function Navbar() {
   return (
     <Container>
-      <Left>HanaTerra.</Left>
+      <Left>
+        <Logo src={LogoPng} alt='Logo Hanaterra'></Logo>HanaTerra.
+      </Left>
       <Center>
         <List>
           <Item>Home</Item>
@@ -16,7 +19,9 @@ function Navbar() {
         </List>
       </Center>
       <Right>
-        <MenuIcon />
+        <Hamburger>
+          <MenuIcon />
+        </Hamburger>
       </Right>
     </Container>
   )
@@ -38,6 +43,14 @@ const Left = styled.div`
   flex: 1;
   font-weight: bold;
   padding-left: 10px;
+  display: flex;
+  align-items: center;
+`
+
+const Logo = styled.img`
+  width: 25px;
+  height: 25px;
+  margin-right: 5px;
 `
 
 const Center = styled.div`
@@ -57,4 +70,8 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+`
+
+const Hamburger = styled.div`
+  display: none;
 `
