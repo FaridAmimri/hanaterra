44150627e575b786cab8756cb516400f1928f244
Farid Amimri <farid.amimri@gmail.com>
2023-02-07 12:06:02 +0100

    Update content & picto

--- src/data.js
+++ src/data.js
@@ -4,8 +4,9 @@ import PhotoSizeSelectActualIcon from '@mui/icons-material/PhotoSizeSelectActual
 import CropIcon from '@mui/icons-material/Crop'
 import HomeIcon from '@mui/icons-material/Home'
 
-import ImageSearchIcon from '@mui/icons-material/ImageSearch'
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'
+import CalendarIcon from './assets/calendar.png'
+import SearchIcon from './assets/search.png'
+import BuildingIcon from './assets/building.png'
 
 export const introData = [
   {
@@ -31,22 +32,22 @@ export const introData = [
 export const serviceData = [
   {
     id: 1,
-    picto: <CalendarMonthIcon />,
+    picto: CalendarIcon,
     title: 'Prenez un rdv avec un de nos agents',
     description:
       'Un de nos agents vous accompagne pour répondre à toutes vos questions par téléphone, whatsapp ou email'
   },
   {
     id: 2,
-    picto: <ImageSearchIcon />,
+    picto: SearchIcon,
     title: 'Visitez sur place nos 100 lots disponibles',
     description:
       'Une première phase de commercialisation avec 100 lots disponibles sur le site de 69 hectares '
   },
   {
     id: 3,
-    picto: <HomeIcon />,
-    title: 'Réservez votre maison de rêve',
+    picto: BuildingIcon,
+    title: 'Réservez pour votre projet de rêve',
     description:
       'Pour les 100 lots, un prix exceptionnel de 3000 € avec une réservation à seulement 1000 €'
   }
